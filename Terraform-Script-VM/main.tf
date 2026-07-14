b9054db0e9c119947dcff1373f5036a731090b6d
rakeshjalagam776 <rakesh.jalagam776@gmail.com>
2026-07-14 11:12:37 +0530

    Add Azure resources for VM networking
    Added virtual network, subnet, public IP, and network security group resources. Updated network interface to attach public IP and associate NSG.

--- Terraform-Script-VM/main.tf
+++ Terraform-Script-VM/main.tf
@@ -1,58 +1,106 @@
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-    name                         = var.resource_group_name
-    location                     = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
-
-resource "azurerm_windows_virtual_machine" "vm" {
-    name                         = var.vm_name
-    resource_group_name          = azurerm_resource_group.rg.name
-    location                     = var.location
-    size                         = var.vm_size
-    admin_username               = var.admin_username
-    admin_password               = var.admin_password
-
-    network_interface_ids        = [azurerm_network_interface.nic.id]
-
-    os_disk {
-        caching                  = "ReadWrite"
-        storage_account_type     = "Standard_LRS"
-    }
-
-    source_image_reference {
-        publisher                = "MicrosoftWindowsServer"
-        offer                    = "WindowsServer"
-        sku                      = "2022-Datacenter"
-        version                  = "latest"
-    }
-}
-
 
 resource "azurerm_virtual_network" "vnet" {
-    name                        = "vm_vnet"
-    address_space               = ["10.0.0.0/16"]
-    location                    = var.location
-    resource_group_name         = azurerm_resource_group.rg.name
+  name                = "vm_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-    name                       = "vm_sunbet"
-    resource_group_name        = azurerm_resource_group.rg.name
-    virtual_network_name       = azurerm_virtual_network.vnet.name
-    address_space              = ["10.0.1.0/24"]
+  name                 = "vm_subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"] # corrected
 }
 
-resource "azurerm_network_interface" "nic" {
-    name                      = "vm_nic"
-    location                  = var.location
-    resource_group_name       = azurerm_resoure_group.rg.name
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+  computer_name         = "vmwin" # valid hostname
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
-    ip_configurarion {
-        name                  = "internal"
-        subnet_id             = azure_subnet.subnet.id
-        private_ip_address_allocation= "Dynamic"
-    }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter"
+    version   = "latest"
+  }
+}
+
+# Public IP (Standard SKU, Static allocation)
+resource "azurerm_public_ip" "pip" {
+  name                = "vm-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"   # ✅ required for Standard SKU
+  sku                 = "Standard" # ✅ Basic is deprecated
+}
+
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Update NIC to attach Public IP
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id   # ✅ attach Public IP
+  }
+}
+
+# Associate NSG with NIC
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }

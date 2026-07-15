bec755d4219a667c50cbe2383c8fc4103bc046bc
rakeshjalagam776 <rakesh.jalagam776@gmail.com>
2026-07-15 10:52:35 +0530

    Refactor VM resources to include Linux VM
    Removed Windows VM configuration and added Linux VM resource.

--- Terraform-Script-VM/main.tf
+++ Terraform-Script-VM/main.tf
@@ -18,30 +18,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "vm_subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"] # corrected
-}
-
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = var.vm_name
-  computer_name         = "vmwin" # valid hostname
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = var.location
-  size                  = var.vm_size
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-datacenter"
-    version   = "latest"
-  }
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Public IP (Standard SKU, Static allocation)
@@ -49,10 +26,9 @@ resource "azurerm_public_ip" "pip" {
   name                = "vm-pip"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"   # ✅ required for Standard SKU
-  sku                 = "Standard" # ✅ Basic is deprecated
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
-
 
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
@@ -61,20 +37,8 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "RDP"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "SSH"
-    priority                   = 1002
+    priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -85,7 +49,7 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
-# Update NIC to attach Public IP
+# NIC with Public IP
 resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
   location            = var.location
@@ -95,7 +59,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id   # ✅ attach Public IP
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
@@ -103,4 +67,36 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.vm_name
+  computer_name         = "vmlinux"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  # Option 1: Password authentication (not recommended)
+  admin_password = var.admin_password
+
+  # Option 2: SSH key authentication (recommended)
+  # admin_ssh_key {
+  #   username   = var.admin_username
+  #   public_key = file("~/.ssh/id_rsa.pub")
+  # }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }

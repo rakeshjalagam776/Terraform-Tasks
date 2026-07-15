f20b0b974df73c92c7605d1d2a9078c835ebdd1c
rakeshjalagam776 <rakesh.jalagam776@gmail.com>
2026-07-15 10:54:47 +0530

    Update output for Virtual Machine details

--- Terraform-Script-VM/output.tf
+++ Terraform-Script-VM/output.tf
@@ -1,10 +1,9 @@
 output "Virtual_Machine_Name" {
-    description = "Name Of The Virtual Marchine"
-    value       = azurerm_windows_virtual_machine.vm.name
+  description = "Name of the Virtual Machine"
+  value       = azurerm_linux_virtual_machine.vm.name
 }
 
 output "vm_public_ip" {
   description = "Public IP of the VM"
   value       = azurerm_public_ip.pip.ip_address
 }
-
